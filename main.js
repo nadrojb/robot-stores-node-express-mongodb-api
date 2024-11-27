941c4a50874db187fea0c86fe29050057ba698d8
jordan_brewer <jordanbrewer1994@gmail.com>
2024-11-27 11:44:07 +0000

    created controller for catergories

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const { MongoClient, ObjectId } = require("mongodb");
 const allProducts = require("./src/Controllers/allProductsController");
 const singleProduct = require("./src/Controllers/singleProductController");
-const categories = require("./src/Controllers/categoriesController");
+const categories = require("./src/Controllers/catergoriesController");
 
 const app = express();
 const port = 3001;
@@ -15,29 +15,32 @@ app.use(cors());
 
 app.get("/", (req, res) => {});
 
-//controller route. all of the below can go into one controller then exported as a group to make it even neater 
+//controller route. all of the below can go into one controller then exported as a group to make it even neater
 app.get("/products", allProducts);
 
 app.get("/products/:id", singleProduct);
 
 app.get("/categories", categories);
-// app.get("/characters", async (req, res) => {
-//   try {
-//     const connection = await MongoClient.connect(url);
 
-//     const db = connection.db("robot_stores");
-//     const collection = db.collection.distinct("products");
+app.get("/characters", async (req, res) => {
+  try {
+    const connection = await MongoClient.connect(url);
 
-//     const character = await collection.distinct("character");
+    const db = connection.db("robot_stores");
+    const collection = db.collection("products");
 
-//     res.status(200).json(character);
-//   } catch (error) {
-//     res.status(500).json({
-//       message: { message: "Unexpected error", data: [] },
-//     });
-//   }
-// });
+    const character = await collection.distinct("character");
 
+    res.status(200).json({
+      message: "Successfully found characters.",
+      data: character,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: { message: "Unexpected error", data: [] },
+    });
+  }
+});
 
 // app.post('/add', async (req, res) => {
 //     const dataToInsert = req.body;
@@ -50,18 +53,18 @@ app.get("/categories", categories);
 //         title: dataToInsert.title
 //         title: dataToInsert.title
 //     };
-    
+
 //     if (!validated) {
 //         return res.status(400).json({message: 'Invalid data.'})
 //     }
-    
+
 //     try {
 //         const connection = await MongoClient.connect(mongourl);
 //         const result = await connection
 //             .db('robot_stores')
 //             .collection('people')
 //             .insertOne(validatedDataToInsert);
-    
+
 //         if (result.insertedId !== null) {
 //             return res.status(201).json({message: 'Success!'})
 //         } else {
@@ -71,8 +74,5 @@ app.get("/categories", categories);
 //         return res.status(500).json({message: 'Something went wrong!', error: error})
 //     }
 // });
-
-
-
 
 app.listen(port);
