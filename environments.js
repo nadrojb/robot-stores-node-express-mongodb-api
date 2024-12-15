e1ffbd7dbdf069e865c3bad0d8824f5c5837fd5e
jordan_brewer <jordanbrewer1994@gmail.com>
2024-12-15 10:25:59 +0000

    updated mongodb link

--- environments.js
+++ environments.js
@@ -1,3 +1,4 @@
-const url = "mongodb+srv://jordan:hello123@robotstores.r6zkh.mongodb.net/?retryWrites=true&w=majority&appName=robotstores";
+const url =
+  "mongodb+srv://jordan:hello123@robotstores.r6zkh.mongodb.net/?retryWrites=true&w=majority&appName=robotstores";
 
 module.exports = url;
